a9c79465dd35b1235952c71ca52ec12feabf00ee
Ben Yarbro <50184801+BenYarbro@users.noreply.github.com>
2019-12-16 19:59:41 -0800

    connect to SQLDB in Azure

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace DotNetCoreSqlDb
 {
     public class Program
-    {
+    { 
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
